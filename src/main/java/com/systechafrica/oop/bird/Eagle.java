9abe01c4cb729f15105227eac0240d0de80e9b20
Tonny Oseko <tonnyoseko0@gmail.com>
2023-10-29 13:34:12 +0300

    Single Responsibility

---
+++ src/main/java/com/systechafrica/oop/bird/Eagle.java
@@ -0,0 +1,8 @@
+package com.systechafrica.oop.bird;
+
+public class Eagle implements Bird {
+    @Override
+    public void fly() {
+        System.out.println("Eagle is flying");
+    }
+}
---
+++ src/main/java/com/systechafrica/oop/bird/Pigeon.java
@@ -0,0 +1,8 @@
+package com.systechafrica.oop.bird;
+
+public class Pigeon implements Bird {
+    @Override
+    public void fly() {
+        System.out.println("Pigeon is flying");
+    }
+}
